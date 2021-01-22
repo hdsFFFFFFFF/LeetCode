5cabb57ad8add2d7a37ffaa36e6e090700773113
hdsFFFFFFFF <hdsFFFFFF@163.com>
2021-01-22 23:00:39 +0800

    modify two_sum.c

--- sample/two-sum/two_sum.c
+++ sample/two-sum/two_sum.c
@@ -1,37 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-	for (int i = 0; i < numsSize; i++) {
-        	int flag = 0;
-                
-		for (int j = i + 1; j < numsSize; j++) {
-        		if (target == nums[i] + nums[j]) {
-                                returnSize[0] = i;
-                                returnSize[1] = j;
-                                int flag = 1; 
-                                
-				break;
+		int *vector = (int *)calloc(2, sizeof(int));
+		if (!vector) {
+			*returnSize = 0;
+			return NULL;
+		}
+
+		for (int i = 0; i < numsSize; i++) {                
+			for (int j = i + 1; j < numsSize; j++) {
+                        	if (target == nums[i] + nums[j]) {
+                                	vector[0] = i;
+                                	vector[1] = j;
+                                	*returnSize = 2;
+                    			return vector;
                         }
                 }
-                
-		if (flag)
-                	break;
         }
-
-        return returnSize;
+        
+    	return NULL;    
 }
 
 int main(void)
 {
-	int nums[] = {1, 2, 3, 4, 5, 6, 7, 8};
-	int numsSize = 8;
-	int target = 9;
-	int returnSize[2] = {0};
+	int nums[] = {1, 2, 3, 4, 5, 6};
+	int numsSize = 6;
+	int target = 11;
+	int returnSize;
+	int *p;
 	
-	twoSum(nums, numsSize, target, returnSize);
+	p = twoSum(nums, numsSize, target, &returnSize);
 	
-	printf("returnSize[0]:%d\n", returnSize[0]);
-	printf("returnSize[1]:%d\n", returnSize[1]);
-
+	for (int i = 0; i < returnSize; i++) {
+		if (i == 0)
+			printf("[");
+		
+		printf("%d", p[i]);
+		
+		if (i != returnSize - 1)
+			printf(",");		
+		
+		if (i == returnSize - 1)
+			printf("]");
+	}
+	
 	return 0;		
 } 
